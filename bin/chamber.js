92e1e16021cad4ce08e05d9ff412f326ad206827
chainreaction6 <blora616@gmail.com>
2022-11-23 16:11:25 -0500

    initializing bin

---
+++ bin/chamber.js
@@ -0,0 +1,8 @@
+const { program } = require("commander");
+
+program
+	.version("1.0.0")
+	.command("swap", "Perform a swap operation"
+	.command("deploy", "Deploy a contract");
+	.command("fetch", "fetch the price of a specific crypto asset")
+	.command("manage", "manage a deployed chamber");
