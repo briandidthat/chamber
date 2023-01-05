721f23bb779752fa759ef1859363685b0c652d01
chainreaction6 <blora616@gmail.com>
2023-01-04 20:49:08 -0500

    fix config

---
+++ utils/config.ts
@@ -0,0 +1,19 @@
+export enum RequiredConfigVars {
+  SIGNER = "SIGNER",
+  PRIVATE_KEY = "PRIVATE_KEY",
+  MAINNET_RPC_URL = "MAINNET_RPC_URL",
+  NETWORK = "NETWORK",
+  RPC_URL = "RPC_URL",
+}
+
+
+/**
+ * 
+  SIGNER: '0x391A377eAF938Ba995a8367d92feB1132909A5DC',
+  PRIVATE_KEY: '0xf298caaef72969e771a3d355e49344f215ec526cfc94324d866e5cec56321b6e',
+  ALCHEMY_RPC_URL: 'https://eth-mainnet.g.alchemy.com/v2/92JlIqALUBPPYpIRjcGfer0Zzw-fma3B',
+  MORALIS_API_KEY: 'Qok1bPfzOT2oOpjgbzYhZiSRRHb8APsKt9RaV8509fdx02ivScPf3mDMWDl1K0n7',
+  RPC_URL: 'https://eth-mainnet.g.alchemy.com/v2/92JlIqALUBPPYpIRjcGfer0Zzw-fma3B',
+  NETWORK: '1',
+  MAINNET_RPC_URL: 'https://eth-mainnet.g.alchemy.com/v2/92JlIqALUBPPYpIRjcGfer0Zzw-fma3B'
+ */
