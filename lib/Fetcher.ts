ac6d25c36a8694eb4524be81f9bfa73c2830fd44
chainreaction6 <blora616@gmail.com>
2023-01-08 19:00:34 -0500

    price endpoint

--- lib/Fetcher.ts
+++ lib/Fetcher.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
-import { ethers, providers } from "ethers";
 import { createQueryString } from "../utils";
-import keyManager from "./KeyManager";
 
 class Fetcher {
   static coinbaseUrl = "https://api.coinbase.com/v2";
 
-  static async getPrice(ticker: string) {
+  static async getExchangeRate(ticker: string) {
     try {
       const response = (
         await axios.get(
@@ -16,6 +14,19 @@ class Fetcher {
         )
       ).data;
       return response.data.rates["USD"];
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  static async getPrice(ticker: string) {
+    try {
+      const response = (
+        await axios.get(
+          `${this.coinbaseUrl}/prices/${ticker}-USD/buy`
+        )
+      ).data;
+      return response.data.amount;
     } catch (err) {
       console.error(err);
     }
